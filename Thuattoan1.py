e4ace11c6d55048dc1559d32e8847a997c452f81
QuangIT89 <88227495+QuangIT89@users.noreply.github.com>
2021-11-28 21:22:44 +0700

    Update Thuattoan1.py

--- Thuattoan1.py
+++ Thuattoan1.py
@@ -1,26 +1,52 @@
-
-one = np.ones((X.shape[0], 1))
-Xbar = np.concatenate((one, X), axis = 1)
-
-# Calculating weights of the fitting line
-A = np.dot(Xbar.T, Xbar)
-b = np.dot(Xbar.T, y)
-w = np.dot(np.linalg.pinv(A), b)
-print('w = ', w)
-# Preparing the fitting line
-w_0 = w[0][0]
-w_1 = w[1][0]
-x0 = np.linspace(145, 185, 2)
-y0 = w_0 + w_1*x0
-
-# Drawing the fitting line
-plt.plot(X.T, y.T, 'ro')     # data
-plt.plot(x0, y0)               # the fitting line
-plt.axis([140, 190, 45, 75])
-plt.xlabel('Height (cm)')
-plt.ylabel('Weight (kg)')
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets, linear_model, metrics
+ 
+# load the boston dataset
+boston = datasets.load_boston(return_X_y=False)
+ 
+# defining feature matrix(X) and response vector(y)
+X = boston.data
+y = boston.target
+ 
+# splitting X and y into training and testing sets
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4,
+                                                    random_state=1)
+ 
+# create linear regression object
+reg = linear_model.LinearRegression()
+ 
+# train the model using the training sets
+reg.fit(X_train, y_train)
+ 
+# regression coefficients
+print('Coefficients: ', reg.coef_)
+ 
+# variance score: 1 means perfect prediction
+print('Variance score: {}'.format(reg.score(X_test, y_test)))
+ 
+# plot for residual error
+ 
+## setting plot style
+plt.style.use('fivethirtyeight')
+ 
+## plotting residual errors in training data
+plt.scatter(reg.predict(X_train), reg.predict(X_train) - y_train,
+            color = "green", s = 10, label = 'Train data')
+ 
+## plotting residual errors in test data
+plt.scatter(reg.predict(X_test), reg.predict(X_test) - y_test,
+            color = "blue", s = 10, label = 'Test data')
+ 
+## plotting line for zero residual error
+plt.hlines(y = 0, xmin = 0, xmax = 50, linewidth = 2)
+ 
+## plotting legend
+plt.legend(loc = 'upper right')
+ 
+## plot title
+plt.title("Residual errors")
+ 
+## method call for showing the plot
 plt.show()
-
-# ketqua
-# w =  [[-33.73541021]
-#  [  0.55920496]]
