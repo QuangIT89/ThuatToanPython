48a4fe6d37ab4d49535296f8ba93dbb5fcd93891
QuangIT89 <88227495+QuangIT89@users.noreply.github.com>
2021-11-28 21:22:03 +0700

    Update ThuatToan.py

--- ThuatToan.py
+++ ThuatToan.py
@@ -1,13 +1,54 @@
-from __future__ import division, print_function, unicode_literals
 import numpy as np
 import matplotlib.pyplot as plt
-# height (cm)
-X = np.array([[147, 150, 153, 158, 163, 165, 168, 170, 173, 175, 178, 180, 183]]).T
-# weight (kg)
-y = np.array([[ 49, 50, 51,  54, 58, 59, 60, 62, 63, 64, 66, 67, 68]]).T
-# Visualize data
-plt.plot(X, y, 'ro')
-plt.axis([140, 190, 45, 75])
-plt.xlabel('Height (cm)')
-plt.ylabel('Weight (kg)')
-plt.show()
+ 
+def estimate_coef(x, y):
+    # number of observations/points
+    n = np.size(x)
+ 
+    # mean of x and y vector
+    m_x = np.mean(x)
+    m_y = np.mean(y)
+ 
+    # calculating cross-deviation and deviation about x
+    SS_xy = np.sum(y*x) - n*m_y*m_x
+    SS_xx = np.sum(x*x) - n*m_x*m_x
+ 
+    # calculating regression coefficients
+    b_1 = SS_xy / SS_xx
+    b_0 = m_y - b_1*m_x
+ 
+    return (b_0, b_1)
+ 
+def plot_regression_line(x, y, b):
+    # plotting the actual points as scatter plot
+    plt.scatter(x, y, color = "m",
+               marker = "o", s = 30)
+ 
+    # predicted response vector
+    y_pred = b[0] + b[1]*x
+ 
+    # plotting the regression line
+    plt.plot(x, y_pred, color = "g")
+ 
+    # putting labels
+    plt.xlabel('x')
+    plt.ylabel('y')
+ 
+    # function to show plot
+    plt.show()
+ 
+def main():
+    # observations / data
+    x = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    y = np.array([1, 3, 2, 5, 7, 8, 8, 9, 10, 12])
+ 
+    # estimating coefficients
+    b = estimate_coef(x, y)
+    print("Estimated coefficients:\nb_0 = {}  \
+          \nb_1 = {}".format(b[0], b[1]))
+ 
+    # plotting regression line
+    plot_regression_line(x, y, b)
+ 
+if __name__ == "__main__":
+    main()
